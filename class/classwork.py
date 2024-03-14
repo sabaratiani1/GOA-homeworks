8962fae60801241f5e1a662a6fa6428469d6d20f
sabaratiani1 <skopaliani95@gmail.com>
2024-03-14 11:16:54 -0700

    commt

--- class/classwork.py
+++ class/classwork.py
@@ -1,7 +1,88 @@
-name = "saba"
+# name = "saba"
 
-last_name = "ratiani"
+# last_name = "ratiani"
 
-my_age = 15
+# my_age = 15
 
-print(type(my_age))
+# print(type(my_age))
+1#favourite_movies=["the shawshank redemption", "the godfather","the dark knight", "pulp fiction" ]
+#print(favourite_movies)
+
+2#films=["the shawshank redemption", "the godfather","the dark knight", "pulp fiction" ]
+#print(films)
+
+3#films=["the shawshank redemption", "the godfather","the dark knight", "pulp fiction" ]
+#favourite_movie = films[0]
+#print("favourite movie", favourite_movie)
+
+4#films=["the shawshank redemption", "the godfather","the dark knight", "pulp fiction" ]
+
+#last_movie = films[-1]
+#second_last_movie = films[-2]
+#print("last movie:", last_movie)
+#print("second last movie:",second_last_movie)
+
+#favourite_brand = (input("what is your favourite car brand"))
+#car_brands = [favourite_brand]
+#print(car_brands)
+
+#numbers = list(range(51))
+
+
+#print("Numbers from 0 to index 25 inclusive:")
+#print(numbers[:26
+
+# def repeat_iteration(num):
+#     for i in range(num):
+#         print("num")
+
+# repeat_iteration(2)
+# repeat_iteration(3)
+# repeat_iteration(4)
+
+# def add(num1, num2):
+#     result = num1 + num2
+#     return result
+
+# def subtract(num1, num2):
+#     result = num1 - num2
+#     return result
+
+# def multiply(num1, y):   
+#     result = num1 * num2
+#     return result
+
+# def divide(num1, num2):    
+#     if num2 != 0:
+#         result = num1 / num2
+#         return result
+#     else:
+#          return "Error: Division by zero is not allowed."
+
+# num1 = 10
+# num2 = 5
+
+# print("Addition result:", add(num1, num2))
+# print("Subtraction result:", subtract(num1, num2))
+# print("Multiplication result:", multiply(num1, num2))
+# print("Division result:", divide(num1, num2))
+
+# def convert_distance():
+#     print("1. Kilometer to Mile")
+#     print("2. Mile to Kilometer")
+#     choice = input("Enter your choice (1 or 2): ")
+
+#     if choice == '1':
+#         km = float(input("Enter kilometers: "))
+#         miles = km * 0.621371
+#         print(f"{km} kilometers is equal to {miles} miles")
+#     elif choice == '2':
+#         miles = float(input("Enter miles: "))
+#         km = miles * 1.60934
+#         print(f"{miles} miles is equal to {km} kilometers")
+#     else:
+#         print("Wrong decision")
+# convert_distance()
+
+
+2
---
+++ day 16/.py
@@ -0,0 +1,8 @@
+print(input("Ra kaci xar"))
+if "answer" == "torti":
+    print("fulis axeva")
+elif "answer" == "kai biwi":
+    print("dazmakaceba")
+else:
+    print("cema")
+
