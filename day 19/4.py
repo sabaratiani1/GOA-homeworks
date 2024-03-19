839731fee3570f67623aa55a0cc305a033f4edda
sabaratiani1 <skopaliani95@gmail.com>
2024-03-19 08:52:50 -0700

    commit

---
+++ day 19/4.py
@@ -0,0 +1,13 @@
+#Abbreviate a Two Word Name
+def abbrev_name(name):
+    name = name.upper()
+    splited_name = name.split(" ")
+    
+    firstname = splited_name[0]
+    lastname = splited_name[1]
+    
+    result = firstname[0] + "." + lastname[0]
+    
+    return result
+
+print(abbrev_name("saba ratiani"))
